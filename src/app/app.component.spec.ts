e124c8a5e5c30d6940b43f41a2bdbd05979342dd
tzmanics <tzmanics@gmail.com>
2021-09-02 14:40:43 -0400

    changes project name for clarification

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sandbox-ng-uni-plugin'`, () => {
+  it(`should have as title 'uni-plugin-ui-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('sandbox-ng-uni-plugin');
+    expect(app.title).toEqual('uni-plugin-ui-demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sandbox-ng-uni-plugin app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain(
+      'uni-plugin-ui-demo app is running!'
+    );
   });
 });
